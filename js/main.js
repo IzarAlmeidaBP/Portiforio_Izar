74deee25d50212c53510707f6f427ab16e579bcb
Izar Almeida Barreto Paes <izaralmeida777@gmail.com>
2023-05-30 00:19:13 -0300

    feat: apenas corrigindo a url

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ const instagram = document.querySelector("#img-instagram");
 const github = document.querySelector("#img-github");
 
 linkedin.addEventListener("click", () => {
-  window.open("https://www.linkedin.com/in/izar-almeida-b78595235/", "_blank");
+  window.open(
+    "https://www.linkedin.com/in/izar-almeida-barreto-paes-b78595235/",
+    "_blank"
+  );
 });
 instagram.addEventListener("click", () => {
   window.open("https://www.instagram.com/izaralmeida/", "_blank");
